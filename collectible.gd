522585840adc2e1beb24bd217c6ed38508f11296
Santiago Rojas <santirojastr12@gmail.com>
2025-10-29 23:09:49 -0500

    GameManager Singleton

--- collectible.gd
+++ collectible.gd
@@ -1,7 +1,6 @@
 extends Area2D
-@onready var game_manager: Node2D = %GameManager
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "CharacterBody2D"):
 		queue_free()
-		game_manager.add_puntos()
+		SingleGameManager.add_puntos()
